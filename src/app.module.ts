48cca6d2c32c754b176baf1214c84cb3dde91001
joycesaquino <joyce.aquino@quintoandar.com.br>
2025-02-24 20:53:14 -0300

    Add domain, infrastructure, and use-case modules.
    Implemented modules for domain and infrastructure layers, including submodules for projects, tasks, and users. Integrated these modules into the app structure and added dependencies like TypeORM and sqlite3 to support database functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ControllersModule } from './gateways/controllers/controllers.module';
+import { DomainModule } from './domain/domain.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { GatewaysModule } from './gateways/gateways.module';
+import { GetAllProjectsService } from './–flat/domain/use-cases/projects/get-all-projects/get-all-projects.service';
 
 @Module({
-  imports: [ControllersModule],
+  imports: [ControllersModule, DomainModule, InfrastructureModule, GatewaysModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GetAllProjectsService],
 })
 export class AppModule {}
