07ba2013617bc8b80ec31621e32680e17fa8c981
joycesaquino <joyce.aquino@quintoandar.com.br>
2025-02-24 20:15:47 -0300

    Add domain interfaces for User, Project, and Task

---
+++ src/domain/interfaces/user.interface.ts
@@ -0,0 +1,13 @@
+// src/domain/interfaces/user.interface.ts
+import { IProject } from './project.interface';
+import { ITask } from './task.interface';
+
+export interface IUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  projects: IProject[];
+  tasks: ITask[];
+}
