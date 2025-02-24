81f6645cfd8e77ccd03ee8648b9ae27f8e8857bb
joycesaquino <joyce.aquino@quintoandar.com.br>
2025-02-24 20:29:24 -0300

    Add documentation comments to domain interfaces

--- src/domain/interfaces/project.interface.ts
+++ src/domain/interfaces/project.interface.ts
@@ -1,6 +1,16 @@
 import { ITask } from './task.interface';
 import { IUser } from './user.interface';
 
+/**
+ * Represents a project with its associated properties and relationships.
+ *
+ * @interface IProject
+ * @property {number} id - The unique identifier of the project.
+ * @property {string} name - The name of the project.
+ * @property {string} description - A detailed description of the project.
+ * @property {ITask[]} tasks - A collection of tasks associated with the project.
+ * @property {IUser} user - The user associated with or responsible for the project.
+ */
 export interface IProject {
   id: number;
   name: string;
--- src/domain/interfaces/user.interface.ts
+++ src/domain/interfaces/user.interface.ts
@@ -2,6 +2,24 @@
 import { IProject } from './project.interface';
 import { ITask } from './task.interface';
 
+/**
+ * Interface representing a user within the system.
+ *
+ * This interface outlines the structure of a user entity,
+ * containing their personal information and associated data.
+ *
+ * Properties:
+ * - id: Unique identifier for the user.
+ * - firstName: First name of the user.
+ * - lastName: Last name of the user.
+ * - email: Email address of the user.
+ * - password: Encrypted password of the user.
+ * - projects: Array of projects associated with the user.
+ * - tasks: Array of tasks assigned to the user.
+ *
+ * This interface is primarily utilized to model user-related data within the application,
+ * facilitating communication between different modules and the database.
+ */
 export interface IUser {
   id: number;
   firstName: string;
