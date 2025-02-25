d915fc8e825483d443d0f5d2729bed01495271f3
joycesaquino <joyce.aquino@quintoandar.com.br>
2025-02-24 21:55:02 -0300

    Add repository services and database module with SQLite config

---
+++ src/domain/repositories/users.repository.service.ts
@@ -0,0 +1,21 @@
+import { DataSource, DeepPartial, Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { IUsersRepository } from './users-repository.interface';
+import { IUser } from '../interfaces/user.interface';
+
+export class UsersRepositoryService
+  extends Repository<UserEntity>
+  implements IUsersRepository
+{
+  constructor(dataSource: DataSource) {
+    super(UserEntity, dataSource.createEntityManager());
+  }
+
+  add(payload: DeepPartial<IUser>): Promise<IUser> {
+    return this.save(payload);
+  }
+
+  findById(id: number): Promise<IUser> {
+    return this.findOneByOrFail( {id} );
+  }
+}
