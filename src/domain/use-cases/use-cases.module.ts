52df283fad1df01b98970805f322debec022f033
joycesaquino <joyce.aquino@quintoandar.com.br>
2025-03-17 19:33:33 -0300

    Implement updates in usage modules

--- src/domain/use-cases/use-cases.module.ts
+++ src/domain/use-cases/use-cases.module.ts
@@ -4,6 +4,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProjectsModule, TasksModule, UsersModule]
+  imports: [ProjectsModule, TasksModule, UsersModule],
+  exports: [ProjectsModule, TasksModule, UsersModule],
 })
 export class UseCasesModule {}
